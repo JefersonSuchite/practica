39c19ccb34f2c46480a269ef7a85e5825e180757
JefersonSuchite <jsuchitec1@miumg.edu.gt>
2023-04-01 10:49:22 -0600

    Mi tercer commit, agregando la funcion multiplicar con float

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@ using namespace std;
 void saludar();
 void sumar(int a, int b);
 float dividir(int c, int d);
+float multiplicar(int e, int f);
 int main() {
     saludar();
     sumar(5,9);
     dividir(100,4);
+    multiplicar(5,8);
     return 0;
 }
 
@@ -19,4 +21,8 @@ void sumar(int a, int b){
 
 float dividir(int divisor, int dividendo){
     cout<<divisor<<"/"<<dividendo<<"="<<divisor/dividendo<<endl;
+}
+
+float multlicacion(int e, int f){
+    cout<<e<<"x"<<f<<"="<<e*f<<endl;
 }
