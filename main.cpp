66d3937422b29a9d01da425d1806acd097cfc544
JefersonSuchite <jsuchitec1@miumg.edu.gt>
2023-04-01 10:39:48 -0600

    Mi segundo commit, agregar un float para dividir

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 void saludar();
 void sumar(int a, int b);
-
+float dividir(int c, int d);
 int main() {
     saludar();
     sumar(5,9);
+    dividir(100,4);
     return 0;
 }
 
@@ -14,4 +15,8 @@ void saludar(){
 }
 void sumar(int a, int b){
     cout<<a<<"+"<<b<< "="<<a+b<<endl;
+}
+
+float dividir(int divisor, int dividendo){
+    cout<<divisor<<"/"<<dividendo<<"="<<divisor/dividendo<<endl;
 }
